fd01601e9175d69a936bbab0026b29f0f1c9f80c
lyx <1287807632@qq.com>
2022-04-16 22:30:36 +0800

    Changes

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -12,14 +12,37 @@ func NewTag() *Tag {
 	return &Tag{}
 }
 
+// @Summary 创建标签
+// @Produce json
+// @Param name query string true "标签名"
+// @Param Status query int true "标签状态"
+// @Param created_by query string true "创建人"
+// @Success 200 {object} model.Tag "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/tag [get]
 func (this *Tag) TagCreat(r *gin.Context) {
 
 }
 
+// @Summary 删除标签
+// @Produce json
+// @Param id query int true "标签id"
+// @Success 200 {object} model.Tag "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/tag{id} [delete]
 func (this *Tag) TagDelete(r *gin.Context) {
 
 }
 
+// @Summary 更新标签
+// @Produce json
+// @Param id query int true "标签id"
+// @Success 200 {object} model.Tag "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/tag{id} [delete]
 func (this *Tag) TagUpdate(r *gin.Context) {
 
 }
